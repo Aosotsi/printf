317ac7d6f6e2c60a260f869e0b6c03adfc7e8db7
Anta Osotsi <aosotsi@gmail.com>
2023-08-19 16:34:48 +0300

    Finishing first part

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _printf(const char *format, ...) {
+    int i;
+    int count = 0;
+
+    va_list args;
+
+    va_start(args, format);
+
+    for (i = 0; format[i] != '\0'; ++i) {
+        if (format[i] == '%') {
+            switch (format[i + 1]) {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    ++count;
+                    break;
+				case 's': 
+				{
+				    const char *str_arg = va_arg(args, const char *);
+				    fputs(str_arg, stdout);
+				    count += strlen(str_arg);
+				    break;
+				}
+                case '%':
+                    putchar('%');
+                    ++count;
+                    break;
+                default:
+                    break;
+            }
+            ++i; // Move past the conversion specifier
+        } else {
+            putchar(format[i]);
+            ++count;
+        }
+    }
+
+    va_end(args);
+    return count;
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int main() {
+    _printf("Hello, %s! This is a %c example.\n", "buddy", 'C');
+    return 0;
+}
