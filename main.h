d7a3f10fc3a1c28d70f8f612611066a22ae19510
Aosotsi <aosotsi@gmail.com>
2023-08-23 01:13:16 +0300

    Update main.h

--- main.h
+++ main.h
@@ -29,7 +29,7 @@ typedef struct printHandler
 	int (*f)(va_list ap, flags_t *f);
 } ph;
 
-int print_int(va_list l, flags_t *f);
+int print_int(va_list args_list, flags_t *format_flags)
 void print_number(int n);
 int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
@@ -37,7 +37,7 @@ int print_hex(va_list l, flags_t *f);
 int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
 int print_octal(va_list l, flags_t *f);
-char *convert(unsigned long int num, int base, int lowercase);
+char *convert(unsigned long int number, int base, int use_lowercase);
 int _printf(const char *format, ...);
 int (*get_print(char s))(va_list, flags_t *);
 int get_flag(char s, flags_t *f);
@@ -45,6 +45,7 @@ int print_string(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
 int _putchar(char c);
 int _puts(char *str);
+int custom_putchar(char character)
 int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
