db124c07259707875648df046ae129ab7e8964b6
Hamza <iklafen.hamza@gmail.com>
2023-08-22 18:07:28 -0700

    correcting

--- print_custom.c
+++ print_custom.c
@@ -1,91 +1,91 @@
 #include "main.h"
 
 /**
- * print_customS - Non printable characters
+ * print_bigS - Non printable characters
  * (0 < ASCII value < 32 or >= 127) are
  * printed this way: \x, followed by the ASCII code
  * value in hexadecimal (upper case - always 2 characters)
- * @args_list: va_list arguments from _printf
- * @format_flags: pointer to the struct flags that determines
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
  * Return: number of char printed
  */
-int print_customS(va_list args_list, flags_t *format_flags)
+int print_bigS(va_list l, flags_t *f)
 {
 	int i, count = 0;
 	char *res;
-	char *input_str = va_arg(args_list, char *);
+	char *s = va_arg(l, char *);
 
-	(void)format_flags;
-	if (!input_str)
+	(void)f;
+	if (!s)
 		return (_puts("(null)"));
 
-	for (i = 0; input_str[i]; i++)
+	for (i = 0; s[i]; i++)
 	{
-		if (input_str[i] > 0 && (input_str[i] < 32 || input_str[i] >= 127))
+		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
 		{
 			_puts("\\x");
 			count += 2;
-			res = convert_to_string(input_str[i], 16, 0);
+			res = convert(s[i], 16, 0);
 			if (!res[1])
 				count += _putchar('0');
 			count += _puts(res);
 		}
 		else
-			count += _putchar(input_str[i]);
+			count += _putchar(s[i]);
 	}
 	return (count);
 }
 
 /**
- * print_reverse - prints a string in reverse
- * @args_list: argument from _printf
- * @format_flags: pointer to the struct flags that determines
+ * print_rev - prints a string in reverse
+ * @l: argument from _printf
+ * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
  * Return: length of the printed string
  */
-int print_reverse(va_list args_list, flags_t *format_flags)
+int print_rev(va_list l, flags_t *f)
 {
 	int i = 0, j;
-	char *input_str = va_arg(args_list, char *);
+	char *s = va_arg(l, char *);
 
-	(void)format_flags;
-	if (!input_str)
-		input_str = "(null)";
+	(void)f;
+	if (!s)
+		s = "(null)";
 
-	while (input_str[i])
+	while (s[i])
 		i++;
 
 	for (j = i - 1; j >= 0; j--)
-		_putchar(input_str[j]);
+		_putchar(s[j]);
 
 	return (i);
 }
 
 /**
- * print_rot13_custom - prints a string using rot13
- * @args_list: list of arguments from _printf
- * @format_flags: pointer to the struct flags that determines
+ * print_rot13 - prints a string using rot13
+ * @l: list of arguments from _printf
+ * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
  * Return: length of the printed string
  */
-int print_rot13_custom(va_list args_list, flags_t *format_flags)
+int print_rot13(va_list l, flags_t *f)
 {
 	int i, j;
 	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	char *input_str = va_arg(args_list, char *);
+	char *s = va_arg(l, char *);
 
-	(void)format_flags;
-	for (j = 0; input_str[j]; j++)
+	(void)f;
+	for (j = 0; s[j]; j++)
 	{
-		if (input_str[j] < 'A' || (input_str[j] > 'Z' && input_str[j] < 'a') || input_str[j] > 'z')
-			_putchar(input_str[j]);
+		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+			_putchar(s[j]);
 		else
 		{
 			for (i = 0; i <= 52; i++)
 			{
-				if (input_str[j] == rot13[i])
+				if (s[j] == rot13[i])
 					_putchar(ROT13[i]);
 			}
 		}
@@ -95,15 +95,15 @@ int print_rot13_custom(va_list args_list, flags_t *format_flags)
 }
 
 /**
- * print_customPercent - prints a percent
- * @args_list: va_list arguments from _printf
- * @format_flags: pointer to the struct flags in which we turn the flags on
+ * print_percent - prints a percent
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags in which we turn the flags on
  * Return: number of char printed
  */
-int print_customPercent(va_list args_list, flags_t *format_flags)
+int print_percent(va_list l, flags_t *f)
 {
-	(void)args_list;
-	(void)format_flags;
+	(void)l;
+	(void)f;
 	return (_putchar('%'));
 }
 
